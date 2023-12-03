b6fae90ef09268967f27122bc67e00f926a39a29
Noor Adn <muhnooradnassaid@gmail.com>
2023-12-03 21:17:46 +0700

    implement Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <>
-      <></>
-    </>
-  );
+  return <>'G'</>;
 }
 
 export default App;
--- src/start.jsx
+++ src/start.jsx
@@ -1,13 +1,18 @@
 // Import React dependencies
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Import Chakra UI CSS
+import { ChakraProvider } from "@chakra-ui/react";
 
 // Import main react file
-import App from './App.jsx'
+import App from "./App.jsx";
 
 // Manipulate the Virtual DOM & render it
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
