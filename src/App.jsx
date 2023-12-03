e09d04f20123541c09cd38ca677dc1bab79b260d
Noor Adn <muhnooradnassaid@gmail.com>
2023-12-04 05:19:44 +0700

    test with chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { AbsoluteCenter, Box } from "@chakra-ui/react";
 import React from "react";
+import { Editable, EditablePreview, EditableTextarea } from "@chakra-ui/react";
 
 function App() {
-  return <>'G'</>;
+  return (
+    <>
+      <p>Jojo</p>
+      <Editable defaultValue="Take some chakra">
+        <EditablePreview />
+        <EditableTextarea />
+      </Editable>
+    </>
+  );
 }
+
+function Jojo() {}
 
 export default App;
