be05fde146851c232f995b269c0c08f266cc1ebc
Noor Adn <muhnooradnassaid@gmail.com>
2023-12-03 19:43:24 +0700

    delete MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import React from "react";
-import ButtonUsage from "./components/Mui";
 
 function App() {
   return (
     <>
-      {/* <Greet /> */}
-      <ButtonUsage name='Satu' style='contained'/>
-      <ButtonUsage name='Dua'/>
-      <ButtonUsage name='Tiga'/>
-      <ButtonUsage name='Empat' stateColor={'success'} style='contained'/>
-      <ButtonUsage name='Lima'/>
-      <ButtonUsage name='Enam' stateColor={'error'} style='contained'/>
-      <ButtonUsage name='Tujuh'/>
+      <></>
     </>
   );
-}
-
-function Greet() {
-  return <p>Test </p>;
 }
 
 export default App;
